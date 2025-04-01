7979b8dfe3451941d81a7a457f1ae5e3b56a4022
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-01 14:05:40 -0300

    update

---
+++ my-app/navigation/DrawerNavigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import Contato from "../pages/Contato";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+    return (
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Profile" component={Profile} />                    
+            <Drawer.Screen name="Contato" component={Contato} />
+        </Drawer.Navigator>
+    );
+}
